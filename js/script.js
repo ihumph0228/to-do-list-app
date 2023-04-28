8414eac080e872cd983ad34414effab9eb8ef018
ihumph0228 <128190629+ihumph0228@users.noreply.github.com>
2023-04-28 18:37:38 -0500

    Updated to try a different approach than replit

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
-function newItem(){
+function newItem() {
 
-    //javascript
-    //1. Adding a new item to the list of items: 
-       let li = $('<li></li>');
-       let inputValue = $('#input').val();
-       li.appendChild(inputValue);
-    
-       if (inputValue === '') {
-         alert('You must write something!');
-       } else {
-         $('#list').append(li);
-       }
+  // 1. Adding a new item to the list of items:
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
-       // 2. Crossing out an item from the list of items:
-       li.on('dblclick', function() {
-         li.toggleClass('strike');
-    });
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    $('#list').append(li);
+  }
 
-    
-      // 3(i). Adding the delete button "X":
-     let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode('X'));
-    li.append(crossOutButton);
-    
-      // 3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  // 2. Crossing out an item from the list of items:
+  li.on('dblclick', function() {
+    li.toggleClass('strike');
+  });
+
+  // 3(i). Adding the delete button "X":
+  let crossOutButton = $('<button></button>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  // 3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
   crossOutButton.on('click', function() {
     li.addClass('delete');
   });
